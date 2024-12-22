a30d401eb9a9c7050d14d2a02cd268bc779352dc
DavidCastillo <davidcastillorodriguez2005@gmail.com>
2024-12-22 02:20:23 -0500

    Creada la carpeta Data junto a la clase Tile.

---
+++ Scripts/Data/Tile.cs
@@ -0,0 +1,13 @@
+namespace MazeRunner.Scripts.Data
+{
+    public class Tile
+    {
+        public int X { get; }
+        public int Y { get; }
+        public Tile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
