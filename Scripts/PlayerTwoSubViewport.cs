d99846b1a9af4410fcd484c2efd1a9799ac02c1d
DavidCastillo <davidcastillorodriguez2005@gmail.com>
2025-01-27 08:06:22 -0500

    Add 2p multiplayer, split screen, a skillset for each player, blind skill, main menu, players´ editor. Change editor and global logic.

---
+++ Scripts/PlayerTwoSubViewport.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+public partial class PlayerTwoSubViewport : SubViewport
+{
+	[Export] private SubViewport _subViewport;
+	
+	public override void _Ready()
+	{
+		World2D = _subViewport.World2D;
+	}
+}
