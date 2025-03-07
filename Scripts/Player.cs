bf7a54f9d4d328cf4c083c28a2a56b52319690f8
DavidCastillo <davidcastillorodriguez2005@gmail.com>
2025-03-07 14:04:23 -0500

    Change Refactor Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using Godot;
 using MazeRunner.Scripts.Data;
 
@@ -25,7 +26,7 @@ public partial class Player : CharacterBody2D
     public System.Timers.Timer CoolDownTimer = new(1000);
     public enum State { Spawning, Idle, Moving, Winning }
     public State CurrentState { get; set; } = State.Spawning;
-    public enum Condition { None, Spikes, Portal, Sticky }
+    public enum Condition { None, Spikes, Portal, Shock }
     public Condition CurrentCondition { get; private set; }
     public int Energy = 0;
     public int BatteryLife = 0;
@@ -57,130 +58,43 @@ public partial class Player : CharacterBody2D
 
     public override void _Ready()
     {
-        _global = GetNode<Global>("/root/Global");
-        _input = Vector2.Zero;
-        _minPosition = Board.GetConvertedPos(0) - Board.TileSize * (float)Math.Pow(4, -1);
-        _maxPosition = Board.GetConvertedPos(_global.MazeGenerator.Size - 1) + Board.TileSize * (float)Math.Pow(4, -1);
-        _defaultSpeed = _speed * Board.TileSize;
-        _paralizedSpeed = _defaultSpeed * 0.1f;
-        _currentSpeed = _defaultSpeed;
-
-        CurrentState = State.Spawning;
-        CurrentCondition = Condition.None;
-
-        _mute.Timer.Elapsed += OnMutedEvent;
-        _mute.Timer.AutoReset = false;
-
-        _blind.Timer.Elapsed += OnBlindEvent;
-        _blind.Timer.AutoReset = false;
-
-        Spikes.Timer.Elapsed += OnSpikedEvent;
-        Spikes.Timer.AutoReset = false;
-
-        _glare.Timer.Elapsed += OnPredatorEvent;
-        _glare.Timer.AutoReset = false;
-
-        CoolDownTimer.Elapsed += OnCooldownEvent;
-        CoolDownTimer.AutoReset = false;
-        CoolDownTimer.Enabled = true;
-
-        switch (_currentPlayerNum)
-        {
-            case 1:
-                if (_global.PlayerOneName == string.Empty) StrName = "Player One";
-                else StrName = _global.PlayerOneName;
-                SkillNum = _global.PlayerOneSkill;
-                break;
-            case 2:
-                if (_global.PlayerOneName == string.Empty)
-                    StrName = "Player Two";
-                else StrName = _global.PlayerTwoName;
-                SkillNum = _global.PlayerTwoSkill;
-                break;
-            default:
-                throw new Exception();
-        }
-        _nameLabel.Text = StrName;
-
-        BatteryLife = SkillNum switch
-        {
-            1 => _shield.BatteryLife,
-            2 => _portalgun.BatteryLife,
-            3 => _blind.BatteryLife,
-            4 => _mute.BatteryLife,
-            5 => _glare.BatteryLife,
-            _ => 0,
-        };
-        GD.Print(SkillNum);
-
-        Position = new Vector2(Board.GetConvertedPos(_global.MazeGenerator.SpawnerCoord.x), Board.GetConvertedPos(_global.MazeGenerator.SpawnerCoord.y));
+        GetGlobalNode();
+        SetInputZero();
+        SetSpeed();
+        SetStateAndCondition();
+        SetTimers();
+        SetPlayerNum();
+        SetSkillBatteryLife();
+        SetMinMaxAndPos();
     }
     public override void _Input(InputEvent @event)
     {
         if (_playerCamera.CurrentState != PlayerCamera.State.Free)
         {
             _input = Input.GetVector(Leftkey, RightKey, UpKey, DownKey);
-
-            if (CurrentState == State.Spawning) { }
-            else if (_input == Vector2.Zero) CurrentState = State.Idle;
-            else CurrentState = State.Moving;
-
-            if (!_mute.Timer.Enabled && SkillNum != 0)
-            {
-                if (SkillNum == 1
-                    && Input.IsActionPressed(SkillKey)
-                    && Energy == BatteryLife) IsShieldOn = true;
-                else IsShieldOn = false;
-
-                if (SkillNum == 2
-                    && _input != Vector2.Zero
-                    && Input.IsActionJustPressed(SkillKey)
-                    && Energy == BatteryLife)
-                {
-                    IsPortalOn = true;
-                    Energy = 0;
-                }
-                else IsPortalOn = false;
-
-                if (SkillNum == 3
-                    && Input.IsActionJustPressed(SkillKey)
-                    && !_enemy._blind.Timer.Enabled
-                    && !_enemy.IsBlind
-                    && Energy == BatteryLife
-                    && IsInsideRadius(Position, _enemy.Position, _blind.Radius * Board.TileSize))
-                {
-                    _enemy.IsBlind = true;
-                }
-
-                if (SkillNum == 4
-                    && Input.IsActionJustPressed(SkillKey)
-                    && Energy == BatteryLife
-                    && IsInsideRadius(Position, _enemy.Position, _mute.Radius * Board.TileSize))
-                {
-                    _enemy.IsMuted = true;
-                    Energy = 0;
-                }
-
-                if (SkillNum == 5
-                    && Input.IsActionJustPressed(SkillKey)
-                    && IsInsideRadius(Position, _enemy.Position, _glare.Radius * Board.TileSize)
-                    && Energy == BatteryLife)
-                {
-                    _enemy.IsParalized = true;
-                    Energy = 0;
-                }
-            }
+            HandleSpawningInput();
+            HandleSkillInput();
         }
         else _input = Vector2.Zero;
     }
     public override void _Process(double delta)
     {
-        GD.Print(Energy);
-
         _PlayerCoord = Board.LocalToMap(Position);
 
         if (_PlayerCoord.X == _global.MazeGenerator.ExitCoord.x && _PlayerCoord.Y == _global.MazeGenerator.ExitCoord.y) CurrentState = State.Winning;
 
+        HandleCooldownTimer();
+        AnyActiveSkill();
+        CheckSpikes();
+        CheckPortals();
+        CheckShockTraps();
+        ResetToNoneCondition();
+        HandleConditionEffects();
+        HandleStateActions();
+    }
+
+    private void HandleCooldownTimer()
+    {
         if (!CoolDownTimer.Enabled && Energy < BatteryLife)
         {
             CoolDownTimer.Enabled = true;
@@ -189,7 +103,9 @@ public partial class Player : CharacterBody2D
         {
             CoolDownTimer.Enabled = false;
         }
-
+    }
+    private void AnyActiveSkill()
+    {
         if (IsParalized)
         {
             _input = Vector2.Zero;
@@ -218,7 +134,9 @@ public partial class Player : CharacterBody2D
                 IsPortalOn = false;
             }
         }
-
+    }
+    private void CheckSpikes()
+    {
         if (_global.MazeGenerator.Maze[_PlayerCoord.X, _PlayerCoord.Y] is Spikes spikes && spikes.IsActive)
         {
             spikes.Deactivate();
@@ -234,6 +152,10 @@ public partial class Player : CharacterBody2D
             }
         }
 
+    EscapeTrap:;
+    }
+    private void CheckPortals()
+    {
         if (_global.MazeGenerator.Maze[_PlayerCoord.X, _PlayerCoord.Y] is Portal portal && portal.IsActive)
         {
             portal.Deactivate();
@@ -249,26 +171,34 @@ public partial class Player : CharacterBody2D
             }
         }
 
-        if (_global.MazeGenerator.Maze[_PlayerCoord.X, _PlayerCoord.Y] is Shock sticky && sticky.IsActive)
+    EscapeTrap:;
+    }
+    private void CheckShockTraps()
+    {
+        if (_global.MazeGenerator.Maze[_PlayerCoord.X, _PlayerCoord.Y] is Shock shock && shock.IsActive)
         {
-            sticky.Deactivate();
+            shock.Deactivate();
             if (IsShieldOn)
             {
                 Energy = 0;
                 goto EscapeTrap;
             }
-            else if (CurrentCondition != Condition.Sticky) CurrentCondition = Condition.Sticky;
+            else if (CurrentCondition != Condition.Shock) CurrentCondition = Condition.Shock;
         }
-    EscapeTrap:
-
+    EscapeTrap:;
+    }
+    private void ResetToNoneCondition()
+    {
         if (CurrentCondition == Condition.Spikes && !Spikes.Timer.Enabled) CurrentCondition = Condition.None;
 
-        if (CurrentCondition == Condition.Sticky && _directionalKeysPressedCount == Shock.Struggle)
+        if (CurrentCondition == Condition.Shock && _directionalKeysPressedCount == Shock.Struggle)
         {
             _directionalKeysPressedCount = 0;
             CurrentCondition = Condition.None;
         }
-
+    }
+    private void HandleConditionEffects()
+    {
         switch (CurrentCondition)
         {
             case Condition.None:
@@ -280,20 +210,21 @@ public partial class Player : CharacterBody2D
             case Condition.Portal:
                 MoveThroughPortal();
                 break;
-            case Condition.Sticky:
+            case Condition.Shock:
                 GetStuckBySticky();
                 break;
             default:
                 throw new Exception();
         }
-
+    }
+    private void HandleStateActions()
+    {
         switch (CurrentState)
         {
             case State.Spawning:
                 Spawn();
                 break;
             case State.Idle:
-                Idle();
                 break;
             case State.Moving:
                 Move();
@@ -304,7 +235,146 @@ public partial class Player : CharacterBody2D
                 throw new Exception();
         }
     }
+    private void HandleSpawningInput()
+    {
+        if (CurrentState == State.Spawning) { }
+        else if (_input == Vector2.Zero) CurrentState = State.Idle;
+        else CurrentState = State.Moving;
+    }
+    private void HandleShieldInput()
+    {
+        if (SkillNum == 1
+                && Input.IsActionPressed(SkillKey)
+                && Energy == BatteryLife) IsShieldOn = true;
+        else IsShieldOn = false;
+    }
+    private void HandlePortalGunInput()
+    {
+        if (SkillNum == 2
+                && _input != Vector2.Zero
+                && Input.IsActionJustPressed(SkillKey)
+                && Energy == BatteryLife)
+        {
+            IsPortalOn = true;
+            Energy = 0;
+        }
+        else IsPortalOn = false;
+    }
+    private void HandleBlindInput()
+    {
+        if (SkillNum == 3
+                && Input.IsActionJustPressed(SkillKey)
+                && !_enemy._blind.Timer.Enabled
+                && !_enemy.IsBlind
+                && Energy == BatteryLife
+                && IsInsideRadius(Position, _enemy.Position, _blind.Radius * Board.TileSize))
+        {
+            _enemy.IsBlind = true;
+        }
+    }
+    private void HandleMuteInput()
+    {
+        if (SkillNum == 4
+                && Input.IsActionJustPressed(SkillKey)
+                && Energy == BatteryLife
+                && IsInsideRadius(Position, _enemy.Position, _mute.Radius * Board.TileSize))
+        {
+            _enemy.IsMuted = true;
+            Energy = 0;
+        }
+    }
+    private void HandleGlareInput()
+    {
+        if (SkillNum == 5
+                && Input.IsActionJustPressed(SkillKey)
+                && IsInsideRadius(Position, _enemy.Position, _glare.Radius * Board.TileSize)
+                && Energy == BatteryLife)
+        {
+            _enemy.IsParalized = true;
+            Energy = 0;
+        }
+    }
+    private void HandleSkillInput()
+    {
+        if (!_mute.Timer.Enabled && SkillNum != 0)
+        {
+            HandleShieldInput();
+            HandlePortalGunInput();
+            HandleBlindInput();
+            HandleMuteInput();
+            HandleGlareInput();
+        }
+    }
+    private void SetSkillBatteryLife()
+    {
+        BatteryLife = SkillNum switch
+        {
+            1 => _shield.BatteryLife,
+            2 => _portalgun.BatteryLife,
+            3 => _blind.BatteryLife,
+            4 => _mute.BatteryLife,
+            5 => _glare.BatteryLife,
+            _ => 0,
+        };
+        GD.Print("SkillNum: " + SkillNum);
+    }
+    private void SetPlayerNum()
+    {
+        switch (_currentPlayerNum)
+        {
+            case 1:
+                if (_global.PlayerOneName == string.Empty) StrName = "Player One";
+                else StrName = _global.PlayerOneName;
+                SkillNum = _global.PlayerOneSkill;
+                break;
+            case 2:
+                if (_global.PlayerOneName == string.Empty)
+                    StrName = "Player Two";
+                else StrName = _global.PlayerTwoName;
+                SkillNum = _global.PlayerTwoSkill;
+                break;
+            default:
+                throw new Exception();
+        }
+        _nameLabel.Text = StrName;
+    }
+    private void GetGlobalNode() { _global = GetNode<Global>("/root/Global"); }
+    private void SetInputZero() { _input = Vector2.Zero; }
+    private void SetMinMaxAndPos()
+    {
+        _minPosition = Board.GetConvertedPos(0) - Board.TileSize * (float)Math.Pow(4, -1);
+        _maxPosition = Board.GetConvertedPos(_global.MazeGenerator.Size - 1) + Board.TileSize * (float)Math.Pow(4, -1);
+        Position = new Vector2(Board.GetConvertedPos(_global.MazeGenerator.SpawnerCoord.x), Board.GetConvertedPos(_global.MazeGenerator.SpawnerCoord.y));
+    }
+    private void SetSpeed()
+    {
+        _defaultSpeed = _speed * Board.TileSize;
+        _paralizedSpeed = _defaultSpeed * 0.1f;
+        _currentSpeed = _defaultSpeed;
+    }
+    private void SetStateAndCondition()
+    {
+        CurrentState = State.Spawning;
+        CurrentCondition = Condition.None;
+    }
+    private void SetTimers()
+    {
+        _mute.Timer.Elapsed += OnMutedEvent;
+        _mute.Timer.AutoReset = false;
 
+        _blind.Timer.Elapsed += OnBlindEvent;
+        _blind.Timer.AutoReset = false;
+
+        Spikes.Timer.Elapsed += OnSpikedEvent;
+        Spikes.Timer.AutoReset = false;
+
+        _glare.Timer.Elapsed += OnPredatorEvent;
+        _glare.Timer.AutoReset = false;
+
+        CoolDownTimer.Elapsed += OnCooldownEvent;
+        CoolDownTimer.AutoReset = false;
+        CoolDownTimer.Enabled = true;
+    }
     private bool IsInsideRadius(Vector2 pos, Vector2 targetPos, int radius)
     {
         int distance = (int)Math.Sqrt(Math.Pow(pos.X - targetPos.X, 2) + Math.Pow(pos.Y - targetPos.Y, 2));
@@ -352,12 +422,10 @@ public partial class Player : CharacterBody2D
         CurrentCondition = (Condition)_previusCondition;
         _previusCondition = null;
     }
-    private void Idle() { }
     private void Move()
     {
         Velocity = _input * _currentSpeed;
         MoveAndSlide();
-
         Position = new Vector2(Math.Clamp(Position.X, _minPosition, _maxPosition), Math.Clamp(Position.Y, _minPosition, _maxPosition));
     }
     private void Spawn()
